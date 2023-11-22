acbeda035440e9aca6a2cd5ab788c945c34f63c7
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-11-22 22:25:12 +0700

    Add Startup Message

--- src/com/khopan/hackontrol/Hackontrol.java
+++ src/com/khopan/hackontrol/Hackontrol.java
@@ -1,7 +1,11 @@
 package com.khopan.hackontrol;
 
+import java.util.Objects;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Hackontrol {
@@ -14,6 +18,8 @@ public class Hackontrol {
 	private static native String getBotToken();
 
 	private final JDA bot;
+	private final Guild guild;
+	private final TextChannel channel;
 
 	private Hackontrol() throws Throwable {
 		this.bot = JDABuilder.createDefault(Hackontrol.BOT_TOKEN)
@@ -21,6 +27,9 @@ public class Hackontrol {
 				.build();
 
 		this.bot.awaitReady();
+		this.guild = Objects.requireNonNull(this.bot.getGuildById(1173967259304198154L));
+		this.channel = Objects.requireNonNull(this.guild.getTextChannelById(1173967259862048891L));
+		this.channel.sendMessage("Hello, world!").queue();
 	}
 
 	public static void main(String[] args) throws Throwable {
