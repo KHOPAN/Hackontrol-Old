7c01485de4a3e2b267cfd75861bcd0918405fc19
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-11-22 20:11:33 +0700

    Setup Bot

--- src/com/khopan/hackontrol/Hackontrol.java
+++ src/com/khopan/hackontrol/Hackontrol.java
@@ -1,13 +1,29 @@
 package com.khopan.hackontrol;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 public class Hackontrol {
 	static {
-		System.load("D:\\GitHub Repository\\Hackontrol\\Native Library\\x64\\Debug\\Token.dll");
+		System.load("D:\\GitHub Repository\\Hackontrol\\dll\\token.dll");
 	}
+
+	private static final String BOT_TOKEN = Hackontrol.getBotToken();
 
 	private static native String getBotToken();
 
-	public static void main(String[] args) {
-		System.out.println(Hackontrol.getBotToken());
+	private final JDA bot;
+
+	private Hackontrol() throws Throwable {
+		this.bot = JDABuilder.createDefault(Hackontrol.BOT_TOKEN)
+				.enableIntents(GatewayIntent.GUILD_MESSAGES)
+				.build();
+
+		this.bot.awaitReady();
+	}
+
+	public static void main(String[] args) throws Throwable {
+		new Hackontrol();
 	}
 }
